a002117030fcbf79488c7e6b6946169080a04ce1
djzwns <hjjshg@game.hoseo.edu>
2022-02-17 14:17:09 +0900

    Add: Variables and Mutability

---
+++ projects/variables/target/debug/deps/variables-6d9a1caa7d983c1b.d
@@ -0,0 +1,5 @@
+/home/rpi/single-bungle-rust-study/projects/variables/target/debug/deps/variables-6d9a1caa7d983c1b: src/main.rs
+
+/home/rpi/single-bungle-rust-study/projects/variables/target/debug/deps/variables-6d9a1caa7d983c1b.d: src/main.rs
+
+src/main.rs:
---
+++ projects/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/rpi/single-bungle-rust-study/projects/variables/target/debug/variables: /home/rpi/single-bungle-rust-study/projects/variables/src/main.rs
